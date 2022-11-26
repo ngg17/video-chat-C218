1a29dc35a155cc5de7c6c551da488950bb7755ac
Noyonika Gupta <71960903+ngg17@users.noreply.github.com>
2022-11-26 09:15:24 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -21,17 +21,27 @@ navigator.mediaDevices
     .then((stream) => {
         myStream = stream;
         addVideoStream(myVideo, stream);
-        socket.on("user-connected", (userId)=>{
-            connectToNewUser(userId, stream)
-        })
-        peer.on("call", (call)=>{
-            call.answer(stream)
-            const video = document.createElement("video")
-            call.on("stream", (userVideoStream)=>{
-                addVideoStream(video, userVideoStream)
-            })
-        })
+
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+        });
+
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        });
     })
+
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+};
 
 function addVideoStream(video, stream) {
     video.srcObject = stream;
@@ -40,14 +50,6 @@ function addVideoStream(video, stream) {
         $("#video_grid").append(video)
     });
 };
-
-function connectToNewUser(userId, stream) {
-    const call = peer.call(userId, stream)
-    const video = document.createElement("video")
-    call.on("stream", (userVideoStream)=>{
-        addVideoStream(video, userVideoStream)
-    })
-}
 
 $(function () {
     $("#show_chat").click(function () {
@@ -75,34 +77,55 @@ $(function () {
         }
     })
 
-    $("#mute_button").click(function(){
+    $("#mute_button").click(function () {
         const enabled = myStream.getAudioTracks()[0].enabled;
-        if(enabled){
-            myStream.getAudioTracks()[0].enabled = false
-            html = `<i class="fas fa-microphone-slash"></i>`
-            $("#mute_button").toggleClass("background_red")
+        if (enabled) {
+            myStream.getAudioTracks()[0].enabled = false;
+            html = `<i class="fas fa-microphone-slash"></i>`;
+            $("#mute_button").toggleClass("background_red");
             $("#mute_button").html(html)
         } else {
-            myStream.getAudioTracks()[0].enabled = true
-            html = `<i class="fas fa-microphone"></i>`
-            $("#mute_button").toggleClass("background_red")
+            myStream.getAudioTracks()[0].enabled = true;
+            html = `<i class="fas fa-microphone"></i>`;
+            $("#mute_button").toggleClass("background_red");
             $("#mute_button").html(html)
         }
     })
 
-    $("#stop_video").click(function(){
+    $("#stop_video").click(function () {
         const enabled = myStream.getVideoTracks()[0].enabled;
-        if(enabled){
-            myStream.getVideoTracks()[0].enabled = false
-            html = `<i class="fas fa-video-slash"></i>`
-            $("#stop_video").toggleClass("background_red")
+        if (enabled) {
+            myStream.getVideoTracks()[0].enabled = false;
+            html = `<i class="fas fa-video-slash"></i>`;
+            $("#stop_video").toggleClass("background_red");
             $("#stop_video").html(html)
         } else {
-            myStream.getVideoTracks()[0].enabled = true
-            html = `<i class="fas fa-video"></i>`
-            $("#stop_video").toggleClass("background_red")
+            myStream.getVideoTracks()[0].enabled = true;
+            html = `<i class="fas fa-video"></i>`;
+            $("#stop_video").toggleClass("background_red");
             $("#stop_video").html(html)
         }
+    })
+
+    $("#invite_button").clicl(function() {
+        const to = prompt("Enter your email address")
+        let data = {
+            url:window.location.href,
+            to:to
+        }
+        $.ajax({
+            url:"/send-mail",
+            type:"post",
+            data:JSON.stringify(data),
+            dataType:'json',
+            contentType:"application/json",
+            success:function(result) {
+                alert("Invite sent")
+            },
+            error:function(result) {
+                console.log(result.responseJSON)
+            }
+        })
     })
 
 })
--- server.js
+++ server.js
@@ -19,6 +19,18 @@ const peerServer = ExpressPeerServer(server, {
 
 app.use("/peerjs", peerServer);
 
+var nodemailer = require("nodemailer")
+
+const transporter = nodemailer.createTransport({
+    port:465,
+    host:"smtp.gmail.com",
+    auth:{
+        user:'noyonikagg170608@gmail.com',
+        pass:'hstuzoviktpleopx'
+    },
+    secure: true
+})
+
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
 });
@@ -27,10 +39,27 @@ app.get("/:room", (req, res) => {
     res.render("index", { roomId: req.params.room });
 });
 
+app.post("/send-email", (req, res)=>{
+    const to = req.body.to;
+    const url = req.body.url
+    const maildata = {
+        from:"noyonikagg170608@gmail.com",
+        to:to,
+        subject:"Join the video chat with me",
+        html:`<p>Hey there! </p><p>Come and join me for a video chat here. ${url}</p>`
+    }
+    transporter.sendMail(maildata, (error, info)=>{
+        if(error){
+            return console.log(error)
+        }
+        res.status(200).send({message:"Invitation sent!", message_id:info.messageId})
+    })
+})
+
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
-        io.to(roomId).emit("user-connected", userId)
+        io.to(roomId).emit("user-connected", userId);
         socket.on("message", (message) => {
             io.to(roomId).emit("createMessage", message, userName);
         });
